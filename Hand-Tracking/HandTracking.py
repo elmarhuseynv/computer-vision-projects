2ec3810ef9357dc41c8acd0139f5282d82e7a05c
Elmar H <elmarhuseynv@gmail.com>
2023-07-14 09:27:39 +0300

    Update HandTracking.py

--- Hand-Tracking/HandTracking.py
+++ Hand-Tracking/HandTracking.py
@@ -28,6 +28,23 @@ class handDetector():
                                                self.mpHands.HAND_CONNECTIONS)
         return img
 
+    def findPosition(self, img, handNo=0, draw=True):
+
+        lmList = []
+        if self.results.multi_hand_landmarks:
+
+            for id, lm in enumerate(self.myHand.landmark):
+                #print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+            # print(id, cx, cy)
+            lmList.append([id, cx, cy])
+            if draw:
+                cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+
+        return lmList
+
 def main():
      pTime = 0
      cTime = 0
